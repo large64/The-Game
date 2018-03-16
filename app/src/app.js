4b04abf0a74d3551684597c36ae0cca5ce3f77e3
Large64 <zadoridenes@gmail.com>
2018-03-16 12:15:02 +0100

    Add webpack, configure basic pixi canvas

--- app/src/app.js
+++ app/src/app.js
@@ -1,1 +1,6 @@
-alert();
+import * as PIXI from 'pixi.js';
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const app = new PIXI.Application(800, 600, {backgroundColor: 'black'});
+    document.body.appendChild(app.view);
+});
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './app/src/app.js',
+    devtool: 'source-map',
+    output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CopyWebpackPlugin([{from: './app/index.html', to: 'index.html'}])
+    ]
+};
