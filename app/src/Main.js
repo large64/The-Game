7a15670a6b64fe24ff163cc2d3afb92fcd3ab877
Large64 <zadoridenes@gmail.com>
2018-03-18 13:00:17 +0100

    Add Main js

---
+++ app/src/Main.js
@@ -0,0 +1,46 @@
+import * as PIXI from 'pixi.js';
+
+import ParallaxScroller from './ParallaxScroller';
+
+export default class Main {
+    constructor() {
+        this.stage = new PIXI.Container();
+        this.renderer = PIXI.autoDetectRenderer(800, 600, {
+            view: document.getElementById('app'),
+            transparent: false,
+        });
+        this.renderer.backgroundColor = 0x1D1D1D;
+
+        this.parallaxScroller = new ParallaxScroller();
+
+        PIXI.loader
+            .add('assets/sprites/spaceship.json')
+            .add('farBackground', 'assets/images/far_background.png')
+            .load(this.onAssetsLoaded.bind(this));
+    }
+
+    onAssetsLoaded() {
+        this.parallaxScroller.init(this.stage);
+
+        const spaceshipAnimationFrames = [];
+
+        for (let i = 0; i < 4; i++) {
+            spaceshipAnimationFrames.push(PIXI.Texture.fromFrame('spaceship' + i + '.png'));
+        }
+
+        const spaceshipAnimation = new PIXI.extras.AnimatedSprite(spaceshipAnimationFrames);
+        spaceshipAnimation.animationSpeed = 0.3;
+        spaceshipAnimation.play();
+        this.stage.addChild(spaceshipAnimation);
+
+        requestAnimationFrame(this.update.bind(this));
+    }
+
+    update() {
+        this.parallaxScroller.moveViewportXBy(Main.FAR_SCROLL_SPEED);
+        this.renderer.render(this.stage);
+        requestAnimationFrame(this.update.bind(this));
+    }
+}
+
+Main.FAR_SCROLL_SPEED = 2;
