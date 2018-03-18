6fd210754f2d1694b1c0eceeb1c3da346bd27b67
Large64 <zadoridenes@gmail.com>
2018-03-18 13:19:08 +0100

    Extend scroller with close layer

--- app/src/ParallaxScroller.js
+++ app/src/ParallaxScroller.js
@@ -1,21 +1,30 @@
-import FarBackgroundLayer from './FarBackgroundLayer';
 import * as PIXI from "pixi.js";
+
+import FarBackgroundLayer from './FarBackgroundLayer';
+import CloseBackgroundLayer from "./CloseBackgroundLayer";
 
 export default class ParallaxScroller {
     constructor() {
         this.farBackgroundLayer = null;
+        this.closeBackgroundLayer = null;
         this.viewportX = 0;
     }
 
     init(stage) {
         const farBackgroundTexture = PIXI.loader.resources['farBackground'].texture;
         this.farBackgroundLayer = new FarBackgroundLayer(farBackgroundTexture);
+
+        const closeBackgroundTexture = PIXI.loader.resources['closeBackground'].texture;
+        this.closeBackgroundLayer= new CloseBackgroundLayer(closeBackgroundTexture);
+
         stage.addChild(this.farBackgroundLayer);
+        stage.addChild(this.closeBackgroundLayer);
     }
 
     setViewportX(viewportX) {
         this.viewportX = viewportX;
         this.farBackgroundLayer.setViewportX(viewportX);
+        this.closeBackgroundLayer.setViewportX(viewportX);
     }
 
     getViewportX() {
