df137fd6201c974309e4ea22090d0c0b5783e72b
Large64 <zadoridenes@gmail.com>
2018-03-18 12:58:01 +0100

    Change loading of js

--- app/src/app.js
+++ app/src/app.js
@@ -1,15 +1,5 @@
-import * as PIXI from 'pixi.js';
+import Application from './Main';
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    let app = new PIXI.Application(800, 600, {transparent: true});
-    document.body.appendChild(app.view);
-
-    PIXI.loader
-        .add('assets/images/kitten.png')
-        .load(setup);
-
-    function setup() {
-        let kitten = new PIXI.Sprite(PIXI.loader.resources['assets/images/kitten.png'].texture);
-        app.stage.addChild(kitten);
-    }
+document.addEventListener('DOMContentLoaded', () => {
+    new Application();
 });
