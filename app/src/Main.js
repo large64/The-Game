2dc14727ae8359542243a31f65c53adb7f730b78
Large64 <zadoridenes@gmail.com>
2018-03-19 15:09:59 +0100

    Refactor player

--- app/src/Main.js
+++ app/src/Main.js
@@ -2,7 +2,7 @@ import * as PIXI from 'pixi.js';
 
 import ParallaxScroller from './ParallaxScroller';
 import Config from './Config';
-import KeyHandler from "./KeyHandler";
+import Player from "./Player";
 
 export default class Main {
     constructor() {
@@ -23,28 +23,21 @@ export default class Main {
             .load(this.onAssetsLoaded.bind(this));
     }
 
-    static createAnimationFromFrames(numberOfFrames, imageName, imageExtension) {
+    createPlayerSprite(numberOfFrames, imageName, imageExtension) {
         const frames = [];
 
         for (let i = 0; i < numberOfFrames; i++) {
             frames.push(PIXI.Texture.fromFrame(imageName + i + '.' + imageExtension));
         }
 
-        return new PIXI.extras.AnimatedSprite(frames);
+        this.player = new Player(frames);
     }
 
     onAssetsLoaded() {
         this.parallaxScroller.init(this.stage);
 
-        const spaceshipAnimation = Main.createAnimationFromFrames(4, 'spaceship', 'png');
-        spaceshipAnimation.animationSpeed = 0.3;
-        spaceshipAnimation.play();
-        this.player = spaceshipAnimation;
-        this.player.vx = 0;
-        this.player.vy = 0;
-        this.handlePlayerMovement();
-
-        this.stage.addChild(spaceshipAnimation);
+        this.createPlayerSprite(4, 'spaceship', 'png');
+        this.stage.addChild(this.player);
 
         document.body.appendChild(this.app.view);
         this.app.ticker.add((delta) => this.update(delta));
@@ -54,53 +47,6 @@ export default class Main {
         this.parallaxScroller.moveViewportXBy(Main.SCROLL_SPEED);
         this.player.position.x += this.player.vx;
         this.player.position.y += this.player.vy;
-    }
-
-    handlePlayerMovement() {
-        const upKeyHandler = new KeyHandler(38);
-        const downKeyHandler = new KeyHandler(40);
-        const rightKeyHandler = new KeyHandler(39);
-        const leftKeyHandler = new KeyHandler(37);
-
-        upKeyHandler.onPress = () => {
-            this.player.vy = -5;
-        };
-
-        upKeyHandler.onRelease = () => {
-            if (!downKeyHandler.isDown) {
-                this.player.vy = 0;
-            }
-        };
-
-        downKeyHandler.onPress = () => {
-            this.player.vy = 5;
-        };
-
-        downKeyHandler.onRelease = () => {
-            if (!upKeyHandler.isDown) {
-                this.player.vy = 0;
-            }
-        };
-
-        rightKeyHandler.onPress = () => {
-            this.player.vx = 5;
-        };
-
-        rightKeyHandler.onRelease = () => {
-            if (!leftKeyHandler.isDown) {
-                this.player.vx = 0;
-            }
-        };
-
-        leftKeyHandler.onPress = () => {
-            this.player.vx = -5;
-        };
-
-        leftKeyHandler.onRelease = () => {
-            if (!rightKeyHandler.isDown) {
-                this.player.vx = 0;
-            }
-        };
     }
 }
 
---
+++ app/src/Player.js
@@ -0,0 +1,65 @@
+import * as PIXI from "pixi.js";
+
+import KeyHandler from "./KeyHandler";
+
+export default class Player extends PIXI.extras.AnimatedSprite {
+    constructor(frames) {
+        super(frames);
+        this.animationSpeed = Player.ANIMATION_SPEED;
+        this.vx = 0;
+        this.vy = 0;
+        this.play();
+
+        this.setKeyHandlers();
+    }
+
+    setKeyHandlers() {
+        const upKeyHandler = new KeyHandler(38);
+        const downKeyHandler = new KeyHandler(40);
+        const rightKeyHandler = new KeyHandler(39);
+        const leftKeyHandler = new KeyHandler(37);
+
+        upKeyHandler.onPress = () => {
+            this.vy = -Player.MOVING_SPEED;
+        };
+
+        upKeyHandler.onRelease = () => {
+            if (!downKeyHandler.isDown) {
+                this.vy = 0;
+            }
+        };
+
+        downKeyHandler.onPress = () => {
+            this.vy = Player.MOVING_SPEED;
+        };
+
+        downKeyHandler.onRelease = () => {
+            if (!upKeyHandler.isDown) {
+                this.vy = 0;
+            }
+        };
+
+        rightKeyHandler.onPress = () => {
+            this.vx = Player.MOVING_SPEED;
+        };
+
+        rightKeyHandler.onRelease = () => {
+            if (!leftKeyHandler.isDown) {
+                this.vx = 0;
+            }
+        };
+
+        leftKeyHandler.onPress = () => {
+            this.vx = -Player.MOVING_SPEED;
+        };
+
+        leftKeyHandler.onRelease = () => {
+            if (!rightKeyHandler.isDown) {
+                this.vx = 0;
+            }
+        };
+    }
+}
+
+Player.ANIMATION_SPEED = 0.3;
+Player.MOVING_SPEED = 5;
