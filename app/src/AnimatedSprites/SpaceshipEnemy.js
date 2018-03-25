88d2a7f03cec07b67ed2504fca30c5c4b5632656
Large64 <zadoridenes@gmail.com>
2018-03-25 17:10:38 +0200

    Refactor particles

--- app/src/AnimatedSprites/SpaceshipEnemy.js
+++ app/src/AnimatedSprites/SpaceshipEnemy.js
@@ -1,5 +1,7 @@
 import * as PIXI from "pixi.js";
 
+import SpaceshipEnemyEmitterData from '../EmitterData/SpaceshipEnemyEmitterData.json';
+
 export default class SpaceshipEnemy extends PIXI.extras.AnimatedSprite {
     constructor(frames) {
         super(frames);
@@ -7,6 +9,19 @@ export default class SpaceshipEnemy extends PIXI.extras.AnimatedSprite {
         this.vx = SpaceshipEnemy.MOVING_SPEED;
         this.position.y = 200;
         this.play();
+
+        this.particleContainer = new PIXI.particles.ParticleContainer();
+        this.emitter = new PIXI.particles.Emitter(
+            this.particleContainer,
+            [PIXI.loader.resources['spaceshipEnemyParticle'].texture],
+            SpaceshipEnemyEmitterData.config
+        );
+        this.emitter.autoUpdate = true;
+        this.emitter.emit = false;
+    }
+
+    updateParticleContainerPosition() {
+        this.particleContainer.position = this.position;
     }
 }
 
