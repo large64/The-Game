3521a991c168496f97f955beb19d18516831d25f
Large64 <zadoridenes@gmail.com>
2018-03-26 19:14:16 +0200

    Make enemy spaceship move randomly

--- app/src/AnimatedSprites/SpaceshipEnemy.js
+++ app/src/AnimatedSprites/SpaceshipEnemy.js
@@ -1,15 +1,26 @@
 import * as PIXI from "pixi.js";
 
-import SpaceshipEnemyEmitterData from '../EmitterData/SpaceshipEnemyEmitterData.json';
+import BlowUpEmitterData from '../EmitterData/BlowUpEmitterData.json';
 import EmittingAnimatedSprite from "./EmittingAnimatedSprite";
+import Helpers from "../Helpers";
 
 export default class SpaceshipEnemy extends EmittingAnimatedSprite {
     constructor(frames) {
-        super(frames, SpaceshipEnemyEmitterData, [PIXI.loader.resources['spaceshipEnemyParticle'].texture]);
+        super(frames, BlowUpEmitterData, [PIXI.loader.resources['spaceshipEnemyParticle'].texture]);
         this.animationSpeed = SpaceshipEnemy.ANIMATION_SPEED;
         this.vx = SpaceshipEnemy.MOVING_SPEED;
         this.position.y = 200;
+        this.pixelsToMoveVertically = 0;
         this.play();
+
+        setInterval(() => {
+            this.changeSpeed();
+            this.pixelsToMoveVertically = Helpers.getRandomInteger(-1, 1);
+        }, Helpers.getRandomInteger(1, 2) * 1000);
+    }
+
+    changeSpeed() {
+        this.vx = Helpers.getRandomInteger(-0.5, -6);
     }
 }
 
