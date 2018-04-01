0508fb0b59433d48c830cd8ad92f3077d0fe42b0
Large64 <zadoridenes@gmail.com>
2018-04-01 11:44:08 +0200

    Build for production, organize packages

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,22 +1,17 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     entry: './app/src/app.js',
-    devtool: 'source-map',
     output: {
-        filename: 'app.js',
-        path: path.resolve(__dirname, 'dist')
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist')
     },
     plugins: [
         new CopyWebpackPlugin([
             {from: './app/index.html', to: 'index.html'},
             {from: './app/assets', to: 'assets'}
-        ]),
-        new BundleAnalyzerPlugin({
-            analyzerMode: "static"
-        })
+        ])
     ],
     module: {
         rules: [
